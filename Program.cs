8a260d804c67fb075435a51480c7899497ed59bc
Gentlestack <valera_dyshlovoy@mail.ru>
2024-04-29 03:00:24 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] array = {"123", "ABCD", "QWERty", "^-^", "V"};
+string[] new_array = {};
+foreach(string str in array)
+{
+    if (str.Length <= 3)
+    {
+        new_array.Append(str);
+        Console.WriteLine(str);
+    }
+}
